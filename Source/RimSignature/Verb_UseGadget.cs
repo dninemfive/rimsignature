03340d645887028112fbe14ffe7c313107e78474
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-06-08 14:55:20 -0700

    initial work on Subverter and Crasher verbs

--- Source/RimSignature/Verb_UseGadget.cs
+++ Source/RimSignature/Verb_UseGadget.cs
@@ -10,7 +10,7 @@ namespace RimSignature
 {
     public abstract class Verb_UseGadget : Verb
     {
-        CompGadget Comp => EquipmentSource?.TryGetComp<CompGadget>() ?? null;
+        CompGadget Comp => EquipmentSource?.TryGetComp<CompGadget>();
 
         protected override bool TryCastShot()
         {
@@ -22,8 +22,56 @@ namespace RimSignature
     {
         protected override bool TryCastShot()
         {
-            if(!base.TryCastShot()) return false;
-            // do shit
+            if (!base.TryCastShot()) return false;
+            // make turrets join your team
+            if (CurrentTarget.Thing is Building_Turret tur && tur.def.CanHaveFaction) tur.SetFactionDirect(Caster.Faction);
+            // unlock autodoors
+            if (CurrentTarget.Thing is Building_Door door && door.def.CanHaveFaction && door.TryGetComp<CompPower>() != null) door.SetFactionDirect(Caster.Faction);
+            if (CurrentTarget.Thing is Pawn pawn)
+            {
+                // code biocoded weapons and apparel to the caster
+                if(CasterIsPawn) SubvertBiocodedWeapons(pawn, CasterPawn);
+                // if pawn is wearing shield belt, set class to custom inverted version if possible
+                // convert mechanoids to your team
+                // TODO: make this a chance instead of guaranteed, based on the mechanoid's health and max HP/combat strength
+                if (pawn.def.race.FleshType == FleshTypeDefOf.Mechanoid) pawn.SetFactionDirect(Caster.Faction);
+            }
+            CompExplosive explosive = CurrentTarget.Thing.TryGetComp<CompExplosive>();
+            if (explosive != null) explosive.StartWick();
+            return true;
+        }
+        public static void SubvertBiocodedWeapons(Pawn target, Pawn subverter)
+        {
+            Pawn_EquipmentTracker eqTracker = target.equipment;
+            foreach(Thing eq in eqTracker.AllEquipmentListForReading)
+            {
+                // if biocodable and biocoded, code to subverter
+                CompBiocodable biocode = eq.TryGetComp<CompBiocodable>();
+                if (biocode != null && biocode.Biocoded)
+                {
+                    biocode.CodeFor(subverter);
+                    eqTracker.TryDropEquipment(eqTracker.Primary, out ThingWithComps dontcare, target.Position);
+                }
+            }
+            Pawn_ApparelTracker apTracker = target.apparel;
+            foreach(Apparel ap in apTracker.WornApparel)
+            {
+                CompBiocodable biocode = ap.TryGetComp<CompBiocodable>();
+                if(biocode != null && biocode.Biocoded)
+                {
+                    biocode.CodeFor(subverter);
+                    apTracker.TryDrop(ap);
+                }
+            }
+        }
+    }
+    public class Verb_Crash : Verb_UseGadget
+    {
+        protected override bool TryCastShot()
+        {
+            if (!base.TryCastShot()) return false;
+            CompBreakdownable breakdownable = CurrentTarget.Thing.TryGetComp<CompBreakdownable>();
+            if (breakdownable != null) breakdownable.DoBreakdown();
             return true;
         }
     }
